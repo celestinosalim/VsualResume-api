5a71d698f532fb3ce56ace5cdeaf6229988dd0ec
celestinosalim <celestino.salim@gmail.com>
2018-12-13 09:17:03 -0500

    Added JWT, user model, serializer, migrations, tested with postman, getting back a token when signing up & login in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,41 @@
 class ApplicationController < ActionController::API
+    before_action :authorized
+
+    def encode_token(payload)
+        # should store secret in env variable
+        JWT.encode(payload, Rails.application.secrets.secret_key_base.to_s)
+    end
+
+    def auth_header
+        # { Authorization: 'Bearer <token>' }
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            # header: { 'Authorization': 'Bearer <token>' }
+            begin
+                JWT.decode(token, Rails.application.secrets.secret_key_base.to_s, true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized
+        render json: { message: "LOGGED IN" }, status: :unauthorized unless logged_in?
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope "/api" do
+    resources :users, only: [:create]
+    post '/login', to: 'auth#create'
+    get '/profile', to: 'users#profile'
+  end
+
 end
