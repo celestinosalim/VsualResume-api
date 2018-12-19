cc489ef2cb95c3d5b63c6db7e6a6b24b015e3138
celestinosalim <celestino.salim@gmail.com>
2018-12-19 11:50:01 -0500

    dropped database

--- db/migrate/20181213164937_create_profiles.rb
+++ db/migrate/20181213164937_create_profiles.rb
@@ -2,9 +2,9 @@ class CreateProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :profiles do |t|
       t.belongs_to :resume, foreign_key: true
-      t.string :name
-      t.integer :age
-      t.string :location
+      t.string :name 
+      t.integer :age 
+      t.string :location 
       t.string :number
       t.string :profile_picture
       t.string :headline
